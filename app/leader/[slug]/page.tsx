1c3f82209447807cac1cfb7881fd97a37026f833
jia8708 <14483673+jia8708@user.noreply.gitee.com>
2025-04-21 23:06:06 +0800

    修改lint错误

--- app/leader/[slug]/page.tsx
+++ app/leader/[slug]/page.tsx
@@ -1,17 +1,7 @@
-import { notFound } from 'next/navigation'
-import {getLeaderInfo} from '../util'
 import React from "react";
 import IntroducePage from "@/app/leader/[slug]/introduce/page";
 
-export default async function Page({ params }: { params: { slug: string } }) {
-    const data = await params;
-    const leader = await getLeaderInfo(data.slug);
-
-    console.log('leader',leader);
-
-    if (!leader) {
-        notFound()
-    }
+export default function Page() {
 
     return (
         <section className="max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0">
